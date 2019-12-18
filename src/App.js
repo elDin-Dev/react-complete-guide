a6f609a5b49846cf038daa7848a5a9ec1195eeb2
elDin-Dev <alexmaildese-dev@yahoo.com>
2019-12-19 00:38:05 +0100

    dynamizar Person component ussando props

--- src/App.js
+++ src/App.js
@@ -6,11 +6,13 @@ function App() {
   return (
     <div className="App">
       <h1>Hi, I'm a React App</h1>
-      <Person />
+      <Person name="Alex" age="44"/>
 
-      <Person />
+      <Person name="Vicente" age="37">
+        My Hobbies:Racing
+      </Person>
 
-      <Person />
+      <Person name="Mon" age="45"/>
     </div>
   );
 }
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
-const person = () => {
-    return <p>Hi, I'm a person and I am {Math.floor(Math.random() * 30)} years old</p>
+const person = (props) => {
+return <p>Hi, I'm {props.name} and I am {props.age} years old</p>
 }
 
 export default person
