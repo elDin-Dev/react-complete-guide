9ffc9d1d59caddfceaea7956156c64d602c9f014
elDin-Dev <alexmaildese-dev@yahoo.com>
2020-01-03 00:25:04 +0100

    passing arguments with bind

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -3,7 +3,7 @@ import React from 'react';
 const person = (props) => {
 return (
     <div>
-        <p>Hi, I'm {props.name} and I am {props.age} years old</p>
+        <p onClick={props.click}>Hi, I'm {props.name} and I am {props.age} years old</p>
         <p>{props.children}</p>
     </div>
 )
