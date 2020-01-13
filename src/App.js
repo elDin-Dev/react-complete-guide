8920c2782338e499d2e5c7dc52dc3b9908739531
elDin-Dev <alexmaildese-dev@yahoo.com>
2020-01-13 23:26:15 +0100

    render directly a list with a map function

--- src/App.js
+++ src/App.js
@@ -61,17 +61,12 @@ class App extends Component {
 
       persons = (
         <div>
-          <Person
-            name={this.state.persons[0].name}
-            age={this.state.persons[0].age} />
-          <Person
-            name={this.state.persons[1].name}
-            age={this.state.persons[1].age}
-            click={this.switchNameHandler.bind(this, 'Pepe2')}
-            changed={this.nameChangehandler}> Hobbies:Racing</Person>
-          <Person
-            name={this.state.persons[2].name}
-            age={this.state.persons[2].age} />
+          {this.state.persons.map(person => {
+            return <Person  
+            name={person.name}
+            age={person.age} />
+          })}
+
         </div>
 
       );
