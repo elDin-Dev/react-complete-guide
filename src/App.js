d153cd1910df28084253d76aad53067bb5e74f12
elDin-Dev <alexmaildese-dev@yahoo.com>
2020-01-14 23:20:11 +0100

    change dinamycally button background color

--- src/App.js
+++ src/App.js
@@ -59,7 +59,8 @@ class App extends Component {
   render() {
 
     const style = {
-      backgroundColor: 'white',
+      backgroundColor: 'green',
+      color:'white',
       font: 'inherit',
       border: '1x solid blue',
       padding: '8px',
@@ -87,6 +88,8 @@ class App extends Component {
         </div>
 
       );
+
+      style.backgroundColor='red';
 
     }
 
