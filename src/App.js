d43dbe2f383b6340f9b791db6e704a16a03a722f
elDin-Dev <alexmaildese-dev@yahoo.com>
2020-01-16 00:31:18 +0100

    eject config + support css--modules

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './App.css';
+import styleClasses from './App.module.css';
 import Person from './Person/Person';
 
 class App extends Component {
@@ -93,20 +93,20 @@ class App extends Component {
 
     }
 
-    let classes = [];
+    let assignedClasses = [];
     if (this.state.persons.length <= 2) {
-      classes.push('red');
+      assignedClasses.push(styleClasses.red);
     }
 
     if (this.state.persons.length <= 1) {
-      classes.push('bold');
+      assignedClasses.push(styleClasses.bold);
     }
 
 
     return (
-        <div className="App">
+        <div className={styleClasses.App}>
           <h1>Hi, I'm a React App</h1>
-          <p className={classes.join(' ')}>This is really working</p>
+          <p className={assignedClasses.join(' ')}>This is really working</p>
 
           <button
             style={style}
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import './Person.css'
+import styleClasses from './Person.module.css'
 
 const person = (props) => {
 
 
     return (
-        <div className="Person" >
+        <div className={styleClasses.Person} >
             <p onClick={props.click}>Hi, I'm {props.name} and I am {props.age} years old</p>
             <p>{props.children}</p>
             <input type="text" onChange={props.changed} value={props.name}></input>
