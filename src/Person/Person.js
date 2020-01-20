8d05097a73e5a3de579109b6ca459e99b59a6f81
elDin-Dev <alexmaildese-dev@yahoo.com>
2020-01-20 23:14:43 +0100

    error boundaries

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -3,8 +3,18 @@ import styleClasses from './Person.module.css'
 
 const person = (props) => {
 
+    const rnd = Math.random();
+
+    if (rnd > 0.7) {
+        throw new Error('Something went wrong');
+    }    
+
+    console.log('-----------------' + rnd);
 
     return (
+
+
+
         <div className={styleClasses.Person} >
             <p onClick={props.click}>Hi, I'm {props.name} and I am {props.age} years old</p>
             <p>{props.children}</p>
