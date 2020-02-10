71a63db4651ab502ec4847d658de332a862475da
elDin-Dev <alexmaildese-dev@yahoo.com>
2020-02-11 00:16:49 +0100

    returning more than one compomen with REact.Fragment

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, {Component, Fragment} from 'react';
 import styleClasses from './Person.module.css'
 import Aux from '../../../hoc/Auxiliary'
 
@@ -12,12 +12,12 @@ class Person extends Component {
         
         return (
 
-                <Aux>
+                <Fragment>
                     <p onClick={this.props.click}>Hi, I'm {this.props.name} and I am {this.props.age} years old</p>
                     <p>{this.props.children}</p>
                     <input type="text" onChange={this.props.changed} value={this.props.name}></input>
 
-                </Aux>
+                </Fragment>
 
         )
     
