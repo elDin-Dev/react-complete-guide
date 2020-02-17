ad20878bb937045d4d3bfd12fcd88f9034d55080
elDin-Dev <alexmaildese-dev@yahoo.com>
2020-02-17 23:57:16 +0100

    using ref to hold a reference to the created component.

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,37 +1,46 @@
-import React, {Component, Fragment} from 'react';
-import styleClasses from './Person.module.css'
-import Aux from '../../../hoc/Auxiliary'
-import withClass from '../../../hoc/withClass';
-import PropTypes, { func } from 'prop-types';
+import React, { Component, Fragment } from "react";
+import styleClasses from "./Person.module.css";
+import Aux from "../../../hoc/Auxiliary";
+import withClass from "../../../hoc/withClass";
+import PropTypes, { func } from "prop-types";
 class Person extends Component {
+  constructor(props) {
+    super(props);
+    this.inputElementRef = React.createRef();
+  }
 
+  componentDidMount() {
+    //this.inputElement.focus();
+    console.log("[Person.js] componentDidMount..." + this.inputElementRef);
+    this.inputElementRef.current.focus();
+  }
 
+  render() {
+    console.log("[Person.js] rendereing...");
 
-    render() {
-    
-        console.log('[Person.js] rendereing...');
-        
-        return (
-
-                <Aux>
-                    <p onClick={this.props.click}>Hi, I'm {this.props.name} and I am {this.props.age} years old</p>
-                    <p>{this.props.children}</p>
-                    <input type="text" onChange={this.props.changed} value={this.props.name}></input>
-
-                </Aux>
-
-        )
-    
-        
-    }
+    return (
+      <Aux>
+        <p onClick={this.props.click}>
+          Hi, I'm {this.props.name} and I am {this.props.age} years old
+        </p>
+        <p>{this.props.children}</p>
+        <input
+          //ref={(inputEl)=> {this.inputElement=inputEl }}
+          ref={this.inputElementRef}
+          type="text"
+          onChange={this.props.changed}
+          value={this.props.name}
+        />
+      </Aux>
+    );
+  }
 }
 
-
 Person.propTypes = {
-    click:PropTypes.func,
-    name:PropTypes.string,
-    age:PropTypes.number,
-    changed:PropTypes.func,
+  click: PropTypes.func,
+  name: PropTypes.string,
+  age: PropTypes.number,
+  changed: PropTypes.func
 };
 
-export default withClass(Person, styleClasses.Person)
+export default withClass(Person, styleClasses.Person);
